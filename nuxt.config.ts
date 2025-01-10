19f607aa974400505d11833cc308ec0d6ebe9bda
alfred@soidea.co <alfred@soidea.co>
2025-01-10 15:40:02 +0700

    set

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2028-11-01',
+  runtimeConfig: {
+    public: {
+        // If you want to expose any of these variables to the client-side,
+        // prefix them with VITE_ or include them here.
+    },
+    private: {
+        GOOGLE_CREDENTIALS_TYPE: process.env.GOOGLE_CREDENTIALS_TYPE,
+        GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
+        GOOGLE_PRIVATE_KEY_ID: process.env.GOOGLE_PRIVATE_KEY_ID,
+        GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY, // Handle newlines
+        GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_AUTH_URI: process.env.GOOGLE_AUTH_URI,
+        GOOGLE_TOKEN_URI: process.env.GOOGLE_TOKEN_URI,
+        GOOGLE_AUTH_PROVIDER_CERT_URL: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+        GOOGLE_CLIENT_CERT_URL: process.env.GOOGLE_CLIENT_CERT_URL,
+    }
+  },
   devtools: { enabled: true }
 })
--- server/api/savegooglesheet.ts
+++ server/api/savegooglesheet.ts
@@ -12,6 +12,25 @@ async function authenticate() {
     return await auth.getClient();
 }
 
+// async function authenticate() {
+//     const auth = new google.auth.GoogleAuth({
+//         credentials: {
+//             type: process.env.GOOGLE_CREDENTIALS_TYPE,
+//             project_id: process.env.GOOGLE_PROJECT_ID,
+//             private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+//             private_key: process.env.GOOGLE_PRIVATE_KEY, // Handle newlines
+//             client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//             client_id: process.env.GOOGLE_CLIENT_ID,
+//             auth_uri: process.env.GOOGLE_AUTH_URI,
+//             token_uri: process.env.GOOGLE_TOKEN_URI,
+//             auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+//             client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+//         },
+//         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+//     });
+//     return await auth.getClient();
+// }
+
 async function writeData(spreadsheetId, range, values) {
     const authClient = await authenticate(); // Authenticate with Google Sheets API
     const sheets = google.sheets({ version: 'v4', auth: authClient });
